6e107395fab3470ab97d04a8102f1d4b87727762
Gabriel Dunne <gdunne@quilime.com>
2019-07-25 10:29:44 -0700

    Update

---
+++ _scripts/build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cp index.md docs/README.md
+
+jekyll build
--- _scripts/publish.sh
+++ _scripts/publish.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cp index.md docs/README.md
+
 jekyll build
 
 rsync -avz _site/ quilime@quilime.com:/home/quilime/media.quilime.com/pg/ --delete
